49bd6ddfe1089b47a4248e20c7edcfd01003ad73
manavnim0 <manavnim0445@gmail.com>
2025-09-28 04:24:44 +0100

    Create stracture and basic front/back end

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
+type Product = { id: string; name: string; price: number; };
+
+export default function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/products').then(r => setProducts(r.data.data))
+      .catch(() => setProducts([]))
+  }, []);
+
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1> Secure Shop - Frontend (Phase 0) </h1>
+      <ul>
+        {
+          products.map(p => (
+            <li ket={p.id}>{p.name} - ${p.price}</li>
+          ))}
+      </ul>
+    </div >
+  );
+}
+
---
+++ service/product/src/server.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 3000
+
+type Product = { id: string; name: string; price: number; };
+
+const products: Product[] = [
+  { id: "p1", name: "Secure T-shirt", price: 29.99 },
+  { id: "p2", name: "Dev Mug", price: 12.99 },
+]
+
+
+app.get('/health', (_req, res) => res.json({ status: "ok" }));
+
+app.get('/products', (_req, res) => res.json({ data: products }));
+
+
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`Product service listeing on ${PORT}`))
+
+}
+
+export default app;
